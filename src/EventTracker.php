342dc8ba8530b6848bda8fb526bbc7fb2333ceb1
Hüsen Baghayi <hossein.baghayi@gmail.com>
2019-12-04 14:31:21 +0330

    send contact email & event as they are required

--- src/EventTracker.php
+++ src/EventTracker.php
@@ -4,6 +4,26 @@ declare(strict_types=1);
 
 namespace Baghayi\Sendinblue;
 
+use Baghayi\Value\Email;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+
 class EventTracker
 {
+    const EVENT_TRACKER_URI = 'http://xyz.com/xyz';
+    private $http;
+
+    public function __construct(Client $http)
+    {
+        $this->http = $http;
+    }
+
+    public function track(string $event, Email $contact)
+    {
+        $request = new Request('GET', self::EVENT_TRACKER_URI, [], json_encode([
+            'email' => (string) $contact,
+            'event' => $event,
+        ]));
+        $this->http->send($request);
+    }
 }
--- test/EventTrackerTest.php
+++ test/EventTrackerTest.php
@@ -5,6 +5,12 @@ declare(strict_types=1);
 namespace Test;
 
 use Baghayi\Sendinblue\EventTracker;
+use Baghayi\Value\Email;
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\TestCase;
 
 class EventTrackerTest extends TestCase
@@ -15,5 +21,36 @@ class EventTrackerTest extends TestCase
     public function events_tracker_service()
     {
         $this->assertTrue(class_exists(EventTracker::class), 'service does not exists');
+    }
+
+    /**
+    * @test
+    */
+    public function tracks_an_event_occured_by_a_contact()
+    {
+        $container = [];
+        $client = $this->getGuzzleClient($container);
+        $service = new EventTracker($client);
+        $service->track('my_event', new Email('sb@domain.com'));
+
+        $data = $this->getRequestData($container);
+        $this->assertSame('sb@domain.com', $data['email']);
+        $this->assertSame('my_event', $data['event']);
+    }
+
+    private function getGuzzleClient(array &$container): Client
+    {
+        $history = Middleware::history($container);
+        $mockHandler = new MockHandler([ new Response(200) ]);
+        $stack = HandlerStack::create($mockHandler);
+        $stack->push($history);
+        $client = new Client(['handler' => $stack]);
+        return $client;
+    }
+
+    private function getRequestData(array $container)
+    {
+        $body = json_decode((string) $container[0]['request']->getBody(), true);
+        return $body;
     }
 }
